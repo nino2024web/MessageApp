990a9c56283a95089508adcbd7198e69b5903d87
nino2024web <selccat.2024@gmail.com>
2024-08-06 23:14:38 +0900

    controller導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
   root "home#index"
+
   devise_for :users
+
+  resources :conversations, only: [:index, :show, :new, :create] do
+    resources :messages, only: [:create]
+  end
 end

b9afb6171e9f64398af6bafc9775274ca4ee5e23
nino2024web <selccat.2024@gmail.com>
2025-04-10 23:20:58 +0900

    add scroll_controller for smooth scroll

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "controllers" # @0.0.2
+pin "fs" # @2.1.0
+pin "path" # @2.1.0
+pin "process" # @2.1.0

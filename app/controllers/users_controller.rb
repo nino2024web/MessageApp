beefdd2d48b5c4930abbf6b3e111349946b996dd
nino2024web <selccat.2024@gmail.com>
2025-04-13 15:02:48 +0900

    🔧 Turbo検索修正＋UI調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,19 +9,47 @@ class UsersController < ApplicationController
     @friend_requests = current_user.received_friend_requests.where(status: 'pending') # 友達検索リクエスト取得
   end
 
+  # def search
+  #   @search_results = params[:name].present? ? perform_search : []
+  #   render partial: 'layouts/leftSide/search_results', locals: { search_results: @search_results }
+  # end
+
   def search
     @search_results = params[:name].present? ? perform_search : []
-    render partial: 'layouts/leftSide/search_results', locals: { search_results: @search_results }
+    handle_search_response
   end
 
   def start_chat
     friend = User.find(params[:id])
-    chat = Chat.between(current_user, friend)
-    chat ||= Chat.create(user1: current_user, user2: friend, name: "#{current_user.name} & #{friend.name}")
+    chat = find_or_create_chat(friend)
     redirect_to chat_path(chat)
   end
 
   private
+
+  def find_or_create_chat(friend)
+    Chat.between(current_user, friend) ||
+      Chat.create(user1: current_user, user2: friend, name: "#{current_user.name} & #{friend.name}")
+  end
+
+  def handle_search_response
+    respond_to do |format|
+      format.turbo_stream { handle_turbo_stream_response }
+      format.html { handle_html_response }
+    end
+  end
+
+  def handle_turbo_stream_response
+    render turbo_stream: turbo_stream.replace(
+      'search-results',
+      partial: 'layouts/leftSide/search_results',
+      locals: { search_results: @search_results }
+    )
+  end
+
+  def handle_html_response
+    render partial: 'layouts/leftSide/search_results', locals: { search_results: @search_results }
+  end
 
   def perform_search
     blocked_user_ids = current_user.blocked_users.pluck(:id) + current_user.blockers.pluck(:id)
